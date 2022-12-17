acb818c1364331ba29791559452060109e363733
Kopyl Tatyana <yurtochka@yandex.ru>
2022-12-17 04:07:40 +0300

    Desided 3 task

---
+++ Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+Write("Введите номер дня недели: ");
+int x = int.Parse(ReadLine());
+
+if (x == 1)
+    WriteLine("День недели - понедельник.");
+else
+    if (x == 2)
+        WriteLine("День недели - вторник.");
+    else
+        if (x == 3)
+            WriteLine("День недели - среда.");
+        else
+            if (x == 4)
+                WriteLine("День недели - четверг.");
+            else
+                if (x == 5)
+                    WriteLine("День недели - пятница.");
+                else
+                    if (x == 6)
+                        WriteLine("День недели - суббота.");
+                    else
+                        if (x == 7)
+                            WriteLine("День недели - воскресенье.");
+                        else
+                            WriteLine("Число не входит в диапозон!");
