136e1ed43191730d85851c046e0b6ed139aed304
Kopyl Tatyana <yurtochka@yandex.ru>
2022-12-17 03:52:27 +0300

    Desided 2 task

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+Write("Введите первое число: ");
+int a = int.Parse(ReadLine()!);
+
+Write("Введите второе число: ");
+int b = int.Parse(ReadLine());
+
+if (a == b * b) 
+    WriteLine($"Число {a} является квадратом {b}");
+else 
+    WriteLine($"Число {a} не является квадратом {b}");
+    
