ba8494c115194cd40a4c426228a501304015b26d
Kopyl Tatyana <yurtochka@yandex.ru>
2022-12-17 05:05:47 +0300

    Desided 5 task

---
+++ Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+Write("Введите трехзначное число: ");
+int x = int.Parse(ReadLine()!);
+
+if (x > 99 & x < 1000)
+{
+    x = x % 10;
+    WriteLine($"Третий символ числа {x}");
+}
+else 
+    WriteLine("Число не трехзначное!");
